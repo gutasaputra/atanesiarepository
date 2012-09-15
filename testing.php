4273a96474fdc562f33c107e749fd8e261de5dae
gutasaputra <gutasaputra@gmail.com>
2012-09-15 19:17:16 +0700

    perubahan telah terjadi
    perubahan telah terjadi

---
+++ testing.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author			: gutasaputra
+ * @email			: gutasaputra@gmail.com
+ * @description		: testing file with git repository
+ */
+echo "hello world !!!";
+
+
+echo "perubahan telah terjadi";
+/* EOF */
+?>
